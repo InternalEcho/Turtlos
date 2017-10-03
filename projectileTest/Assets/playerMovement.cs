b4c88106f2f93b2174be5cb7da1ac8a2029b12b9
Kaixiang <kaixiangmu@gmail.com>
2017-10-02 23:29:07 -0400

    prototype for shooting projectile

---
+++ projectileTest/Assets/playerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
