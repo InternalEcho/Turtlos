83d06449f97fccede680d79845ac45fcaef3e8e8
BrandonCHW <brad_1800@hotmail.com>
2017-11-23 16:39:32 -0500

    random shit

---
+++ MapTeam/Assets/Scripts/RandomEvents/GenericYvant.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericYvant : MonoBehaviour {
+
+    public virtual void activate() 
+    {
+        Debug.Log("GenericEvent!");
+    }
+
+}
---
+++ MapTeam/Assets/Scripts/RandomEvents/YvantManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YvantManager : MonoBehaviour {
+
+    public GameObject[] genericyvants;
+    Vector3 randpos;
+    public float min, max;
+    public float minSec, maxSec;
+
+    public void DebugEvent()
+    {
+        StartCoroutine(SpawnMultiple());
+    }
+
+    private IEnumerator SpawnMultiple()
+    {
+        
+        for (int nb = 0; nb < genericyvants.Length; ++nb ) { 
+            randpos = new Vector3(Random.Range(min, max), 0, Random.Range(min, max));
+            yield return new WaitForSeconds(Random.Range(minSec,maxSec));
+            GameObject newEvent = Instantiate(genericyvants[nb], randpos, Quaternion.identity) as GameObject;
+        }
+        //newEvent.GetComponent<GenericYvant>().activate();
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
