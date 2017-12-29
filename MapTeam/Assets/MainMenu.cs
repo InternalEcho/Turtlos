6d6a8d9334f6a35193de3a7c24a557a9e349af6e
Kaixiang <kaixiangmu@gmail.com>
2017-12-29 18:45:36 -0500

    refont of main menu

---
+++ MapTeam/Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public void GoToMenu()
+    {
+        GameManagementScript.Instance.GoToMenu();
+    }
+
+    public void GoToGame()
+    {
+        GameManagementScript.Instance.GoToGameStart();
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quit game");
+        Application.Quit();
+    }
+}
