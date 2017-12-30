c2384bb2d44378499a7bb4e4e9e84614bd7efd40
InternalEcho <24857190+InternalEcho@users.noreply.github.com>
2017-12-30 04:24:49 -0500

    PowerUps now flash when they near the end of their lifespans. Added brighter lighting to the map to make the clouds look better.

--- MapTeam/Assets/Scripts/PowerUps/genericPowerUp.cs
+++ MapTeam/Assets/Scripts/PowerUps/genericPowerUp.cs
@@ -6,7 +6,20 @@ public class genericPowerUp : MonoBehaviour {
 
 	public IEnumerator lifeSpan(float powerUpLifeSpan)
     {
-        yield return new WaitForSeconds(powerUpLifeSpan);
+        float lifeSpanAlmostUp = powerUpLifeSpan / 10;  //Start flashing at 1/10 the lifespan of a PowerUp
+        float lifeSpanFlashInterval = lifeSpanAlmostUp / 20;
+        float lifeSpanNormal = lifeSpanAlmostUp * 9;
+        Debug.Log("LifeSpanAlmostUp: " + lifeSpanAlmostUp);
+        Debug.Log("LifeSpanFlashInterval: " + (float) lifeSpanFlashInterval);
+        Debug.Log("LifeSpanNormal: " + lifeSpanNormal);
+        yield return new WaitForSeconds(lifeSpanNormal);
+        for (int i = 0; i < 10; i++)
+        {
+            this.gameObject.GetComponent<Renderer>().enabled = false;
+            yield return new WaitForSeconds(lifeSpanFlashInterval);
+            this.gameObject.GetComponent<Renderer>().enabled = true;
+            yield return new WaitForSeconds(lifeSpanFlashInterval);
+        }
         //Make the powerUp flash when it's about to disappear?
         Destroy(this.gameObject);
     }
